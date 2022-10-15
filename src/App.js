4c477bb5d223f191debda9dcd1f7e74d4947de55
Dangbao051100 <115637171+Dangbao051100@users.noreply.github.com>
2022-10-15 11:40:49 +0700

    aaa

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import {BrowserRouter as Router ,Routes,Route} from "react-router-dom";
 import './App.css';
 import Header from "./Header/Header.js";
-// import Login from "./Login/Login";
-// import Checkout from "./Checkout/Checkout";
 import Home from "./Home/Home";
+//aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 function App() {
   return (
     <Router>
@@ -11,8 +10,6 @@ function App() {
         <Routes>
           <Route path='/' element={<Header/>}/><Route/>
           <Route path='/home' element={<Home/>}/><Route/>
-          {/* <Route path='/login' element={<Login/>}/><Route/>
-          <Route path='/checkout' element={<Checkout/>}/><Route/> */}
         </Routes>
       </div>
     </Router>
